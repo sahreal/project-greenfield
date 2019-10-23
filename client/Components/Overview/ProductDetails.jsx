cffdd4ac338907063decb2299a8126c683f4bd78
Sahar <saharadri1@gmail.com>
2019-10-23 10:50:11 -0400

    overview componentes

---
+++ client/Components/Overview/ProductDetails.jsx
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+
+class ProductDetails extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default ProductDetails
