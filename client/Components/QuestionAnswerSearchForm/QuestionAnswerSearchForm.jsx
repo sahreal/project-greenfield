ef6f7fb1e25f91199e74811eba17e771ba03be5b
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-10-23 12:02:52 -0400

    added global settings, fixed some app settings, and created base components for question answer widget

---
+++ client/Components/QuestionAnswerSearchForm/QuestionAnswerSearchForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QuestionAnswerSearchForm = () => {
+  return (
+    <div className="QuestionAnswerSearchForm-container">
+      Question Answer Search Form
+    </div>
+  );
+};
+
+export default QuestionAnswerSearchForm;
