5ef254a92feab6e131c063047b90632fa3467527
Sahar <saharadri1@gmail.com>
2019-10-23 13:54:13 -0400

    addressed concerns, rendering properly, ignore testing files/folder will address later.

--- client/Components/App/App.jsx
+++ client/Components/App/App.jsx
@@ -2,28 +2,20 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 
 import StarReviews from "../Reviews/StarReviews.jsx";
-<<<<<<< HEAD
+
 import OverView from "../Overview/Overview.jsx";
-=======
->>>>>>> staging
+
 import Homepage from "../Homepage/Homepage.jsx";
 
 const App = () => {
   return (
     <div className="app-container">
-<<<<<<< HEAD
       <Link to="/test/reviews">Reviews</Link>
       <Link to="/test/overview">Overview</Link>
       <Link to="/">Home</Link>
       <Switch>
         <Route path="/test/reviews" exact component={StarReviews} />
         <Route path="/test/overview" exact component={OverView} />
-=======
-      <Link to="/test/reviews">Test</Link>
-      <Link to="git/">Home</Link>
-      <Switch>
-        <Route path="/test/reviews" exact component={StarReviews} />
->>>>>>> staging
         {/* <Route to='/:product/:style' exact render={(props)=>{
           return <YOURCOMPONENTHERE someProp={props.something} />
         }} */}
--- client/Components/Overview/Overview.jsx
+++ client/Components/Overview/Overview.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import ImageGallery from "./ImageGallery.jsx";
-import ProductDetails from "./ProductDetails.jsx";
+import ImageGallery from "./ImageGallery/ImageGallery.jsx";
+import ProductDetails from "./ProductDetails/ProductDetails.jsx";
+//import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 const Overview = () => {
   return (
-    <div>
-      <Container>
-        <ImageGallery />
-      </Container>
-      <Container>
-        <ProductDetails />
-      </Container>
+    <div className="Overview">
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper>
+            <ImageGallery />
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>
+            <ProductDetails />
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
