6854725e9af525908010ee081a234a7afd8e3884
Sahar <saharadri1@gmail.com>
2019-10-23 12:12:01 -0400

    updated  app links

--- client/Components/App/App.jsx
+++ client/Components/App/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 
+import StarReviews from "../Reviews/StarReviews.jsx";
 import OverView from "../Overview/Overview.jsx";
-//import Homepage from "../Homepage/Homepage.jsx";
+import Homepage from "../Homepage/Homepage.jsx";
 
 const App = () => {
   return (
     <div className="app-container">
-      <Link to="/test/Overview">Overview</Link>
+      <Link to="/test/reviews">Reviews</Link>
+      <Link to="/test/overview">Overview</Link>
       <Link to="/">Home</Link>
       <Switch>
-        <Route path="/test/Overview" exact component={OverView} />
+        <Route path="/test/reviews" exact component={StarReviews} />
+        <Route path="/test/overview" exact component={OverView} />
         {/* <Route to='/:product/:style' exact render={(props)=>{
           return <YOURCOMPONENTHERE someProp={props.something} />
         }} */}
-        {/* <Route path="/" exact component={Homepage} /> */}
+        <Route path="/" exact component={Homepage} />
       </Switch>
     </div>
   );
