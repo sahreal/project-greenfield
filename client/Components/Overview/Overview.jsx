24c788f3a6cb3aa00e73824a2d5d702d9c74ecd9
Sahar <saharadri1@gmail.com>
2019-10-23 20:06:56 -0400

    added selectorItem for styleselector to map through it later when i make the api call. Updated some button icons and imported the avatar icon

--- client/Components/Overview/Overview.jsx
+++ client/Components/Overview/Overview.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ImageGallery from "./ImageGallery/ImageGallery.jsx";
 import ProductDetails from "./ProductDetails/ProductDetails.jsx";
-//import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import axios from "axios";
 
 const Overview = () => {
+  axios
+    .get("http://18.223.1.30/products/1/styles")
+    .then(axiosResult => console.log(axiosResult.data))
+    .catch(err => console.log(err, "error"));
+
   return (
     <div className="Overview">
       <Grid container spacing={3}>
