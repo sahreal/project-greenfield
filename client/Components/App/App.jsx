63461cdda5bb0aa6564a0be026a8c994feda2693
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-10-24 10:47:35 -0400

    fixed app.js

--- client/Components/App/App.jsx
+++ client/Components/App/App.jsx
@@ -11,37 +11,27 @@ import "./App.css";
 const App = () => {
   return (
     <div className="app-container">
-      <Link to="/test/reviews">Reviews</Link>
-      <Link to="/test/overview">Overview</Link>
-      <Link to="/">Home</Link>
+      <header className="header-container">
+        <ul className="header-ul">
+          <li className="header-li">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="header-li">
+            <Link to="/test/reviews">Test</Link>
+          </li>
+          <li className="header-li">
+            <Link to="/test/overview">Overview</Link>
+          </li>
+          <li className="header-li">
+            <Link to="/test/questionanswer">Question Answer</Link>
+          </li>
+        </ul>
+      </header>
+
       <Switch>
         <Route path="/test/reviews" exact component={StarReviewsContainer} />
         <Route path="/test/overview" exact component={OverView} />
         <Route path="/test/questionanswer" exact component={QuestionAnswer} />
-
-        <header className="header-container">
-          <ul className="header-ul">
-            <li className="header-li">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="header-li">
-              <Link to="/test/reviews">Test</Link>
-            </li>
-            <li className="header-li">
-              <Link to="/test/questionanswer">Question Answer</Link>
-            </li>
-          </ul>
-        </header>
-
-        {/* EXAMPLE ROUTE WITH PROPS 
-        <SHOULD NOT NEED PROPS DUE TO USING REDUX> 
-
-        <Route to='/:product/:style' exact render={(props)=>{
-
-          return <YOURCOMPONENTHERE someProp={props.something} />
-        }}
-
-         END EXAMPLE ROUTE */}
 
         {/* THIS '/' PATH HAS TO BE THE LAST COMPONENT */}
         <Route path="/" exact component={Homepage} />
