8656cb0314d72e18caf4b039782ce799e060f940
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-10-28 10:08:30 -0400

    made into class component for qaList

--- client/Components/QuestionAnswerList/QuestionAnswerList.jsx
+++ client/Components/QuestionAnswerList/QuestionAnswerList.jsx
@@ -2,17 +2,33 @@ import React from "react";
 
 import QuestionAnswerCard from "../QuestionAnswerCard/QuestionAnswerCard.jsx";
 
-const QuestionAnswerList = ({ questions }) => {
-  return (
-    <div className="QuestionAnswerList-container">
-      {questions.map(question => (
-        <QuestionAnswerCard question={question} key={question.question_id} />
-      ))}
-      {/* <QuestionAnswerCard question={questions[0]} /> */}
-      <input type="button" value="MORE ANSWER QUESTIONS" />
-      <input type="button" value="ADD A QUESTION + " />
-    </div>
-  );
-};
-
-export default QuestionAnswerList;
+export default class QuestionAnswerList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+  }
+  render() {
+    return (
+      <div className="QuestionAnswerList-container">
+        {this.props.questions.map((question, index) =>
+          !this.state.isClicked && index < 4 ? (
+            <QuestionAnswerCard
+              question={question}
+              key={question.question_id}
+            />
+          ) : (
+            <QuestionAnswerCard
+              question={question}
+              key={question.question_id}
+            />
+          )
+        )}
+        {/* <QuestionAnswerCard question={questions[0]} /> */}
+        <input type="button" value="MORE ANSWER QUESTIONS" />
+        <input type="button" value="ADD A QUESTION + " />
+      </div>
+    );
+  }
+}
