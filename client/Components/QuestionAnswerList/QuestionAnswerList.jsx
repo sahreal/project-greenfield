971cc2b53a85e028310c35d86f5bbe313760548c
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-10-28 11:25:52 -0400

    page loads up to 4 questions sorted by helpfulness

--- client/Components/QuestionAnswerList/QuestionAnswerList.jsx
+++ client/Components/QuestionAnswerList/QuestionAnswerList.jsx
@@ -12,21 +12,46 @@ export default class QuestionAnswerList extends React.Component {
   render() {
     return (
       <div className="QuestionAnswerList-container">
-        {this.props.questions.map((question, index) =>
-          !this.state.isClicked && index < 4 ? (
-            <QuestionAnswerCard
-              question={question}
-              key={question.question_id}
-            />
-          ) : (
-            <QuestionAnswerCard
-              question={question}
-              key={question.question_id}
-            />
-          )
-        )}
+        {this.props.questions
+          .sort((a, b) => b.question_helpfulness - a.question_helpfulness)
+          .map((question, index) =>
+            // !this.state.isClicked && index < 4 ? (
+            //   <QuestionAnswerCard
+            //     question={question}
+            //     key={question.question_id}
+            //   />
+            // ) : (
+            //   <QuestionAnswerCard
+            //     question={question}
+            //     key={question.question_id}
+            //   />
+            // )
+            {
+              if (
+                this.state.isClicked &&
+                Object.keys(question.answers).length > 0
+              ) {
+                return (
+                  <QuestionAnswerCard
+                    question={question}
+                    key={question.question_id}
+                  />
+                );
+              } else if (
+                index < 4 &&
+                Object.keys(question.answers).length > 0
+              ) {
+                return (
+                  <QuestionAnswerCard
+                    question={question}
+                    key={question.question_id}
+                  />
+                );
+              }
+            }
+          )}
         {/* <QuestionAnswerCard question={questions[0]} /> */}
-        <input type="button" value="MORE ANSWER QUESTIONS" />
+        <input type="button" value="MORE ANSWERED QUESTIONS" />
         <input type="button" value="ADD A QUESTION + " />
       </div>
     );
